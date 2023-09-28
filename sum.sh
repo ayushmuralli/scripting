1b4363e20dbfd8d7b359607b45e890528f156dcc
ayushmurali <ayushvalappil@gmail.com>
2023-09-28 10:09:21 +0530

    sum.sh

---
+++ sum.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo enter the first number
+read num1
+echo enter the second number
+read num2
+echo enter the third number
+read num3
+if [ $num1 -gt $num2 ] && [ $num1 -gt $num3 ]
+then 
+echo $num1 is largest
+elif [ $num2 -gt $num1 ] && [ $num2 -gt $num3 ]
+then
+echo $num2 is largest
+else 
+echo $num3 is the largest
+fi
